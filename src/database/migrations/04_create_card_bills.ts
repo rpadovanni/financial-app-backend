bb03154a9aa5553e20b482643ccebb81cc53f420
Rafael Padovani <r.padovanni@hotmail.com>
2020-07-13 23:46:07 -0300

    Adding missing fields to tables

--- src/database/migrations/04_create_card_bills.ts
+++ src/database/migrations/04_create_card_bills.ts
@@ -3,8 +3,9 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
     return knex.schema.createTable('card_bills', table => {
         table.increments('card_bill_id').primary();
+        table.decimal('total').notNullable();
         table.date('due_date').notNullable();
-        table.integer('card_id').references('card_id').inTable('cards');
+        table.integer('card_id').references('card_id').inTable('cards').notNullable();
         table.timestamps(true, true);
     });
 }
