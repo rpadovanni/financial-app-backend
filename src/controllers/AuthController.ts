a9d535f90bc5e27221b79a22a2ef74bcafe69c12
Rafael Padovani <r.padovanni@hotmail.com>
2020-08-11 00:11:08 -0300

    Saving bcrypted password and generating user token

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import knex from '../database/connection';
+import jwt from 'jsonwebtoken';
+import IUser from '../interfaces/IUser';
+import authConfig from '../config/auth';
+import { generateJwtToken } from '../helpers/authentication.helper';
+
+export default {
+    async authenticate(req: Request, res: Response) {
+        const { email, password } = req.body;
+        
+        const user = await knex('users').where('email', email).first();
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found :(' });
+        }
+
+        if (!(await bcrypt.compare(password, user.password))) {
+            return res.status(404).json({ message: 'Invalid password :/' });
+        }
+
+        const token = generateJwtToken({ id: user.user_id });
+
+        return res.json({ user, token });
+    },
+};
+
--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -5,6 +5,7 @@ export async function up(knex: Knex) {
         table.increments('user_id').primary();
         table.string('name').notNullable();
         table.string('username').notNullable().unique();
+        table.string('email').notNullable().unique();
         table.string('password').notNullable();
         table.timestamps(true, true);
     });
