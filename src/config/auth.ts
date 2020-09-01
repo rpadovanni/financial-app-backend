69829dc788d328aa1161dedcef33ed196a7b3255
Rafael Padovani <r.padovanni@hotmail.com>
2020-08-31 23:34:11 -0300

    Build Fix

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const authConfig = {
-    secret: isProduction ? process.env.JWT_SECRET : "e95d30b3f15ae85b730ea6dda6aec9e5"
+    secret: isProduction ? (process.env.JWT_SECRET as string) : "e95d30b3f15ae85b730ea6dda6aec9e5"
 }
 
 export default authConfig;
