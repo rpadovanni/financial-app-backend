faa4061a3ead86f27ad146c9bfbbd47625b7ec83
Rafael Padovani <r.padovanni@hotmail.com>
2020-08-31 23:31:14 -0300

    Test

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,7 @@ app.use(express.json());
 app.use(authenticationMiddleware);
 app.use(routes);
 
+console.log('Secret: ', process.env.JWT_SECRET);
+console.log('NODE_ENV: ', process.env.NODE_ENV);
+
 app.listen(process.env.PORT || 3333);
