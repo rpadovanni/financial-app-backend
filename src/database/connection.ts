660a6abfd036ba2753b8ff0c9c03d635b926efa5
Rafael Padovani <r.padovanni@hotmail.com>
2020-08-31 22:51:51 -0300

    Connection fix test

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,8 +2,8 @@ import knex from 'knex';
 import path from 'path';
 
 const connection = knex({
-    client: 'sqlite3',
-    connection: {
+    client: process.env.BUILD_ENV || 'sqlite3',
+    connection: process.env.DATABASE_URL || {
         filename: path.resolve(__dirname, 'financial-app-db.sqlite')
     },
     useNullAsDefault: true
